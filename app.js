2120bd0db9146a53a061ba7306da89a9294f1a1a
Alexandru Russe <russealexandru@gmail.com>
2023-08-22 11:28:05 +0300

    Added filter option for all days

--- app.js
+++ app.js
@@ -4,18 +4,50 @@ const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 const filterOptions = document.querySelector('.filter-todo');
 
+const todoInputTuesday = document.querySelector('.todo-input-tuesday');
+const todoButtonTuesday = document.querySelector('.todo-button-tuesday');
+const todoListTuesday = document.querySelector('.todo-list-tuesday');
+
+const todoInputWednesday = document.querySelector('.todo-input-wednesday');
+const todoButtonWednesday = document.querySelector('.todo-button-wednesday');
+const todoListWednesday = document.querySelector('.todo-list-wednesday');
+
+const todoInputThursday = document.querySelector('.todo-input-thursday');
+const todoButtonThursday = document.querySelector('.todo-button-thursday');
+const todoListThursday = document.querySelector('.todo-list-thursday');
+
+const todoInputFriday = document.querySelector('.todo-input-friday');
+const todoButtonFriday = document.querySelector('.todo-button-friday');
+const todoListFriday = document.querySelector('.todo-list-friday');
+
 //DATE VARs
 const dateObj = new Date();
 let dayName =['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 let dayOfWeek = dayName[new Date().getDay()];
 let date =  dateObj.getDate() + '/'+ (dateObj.getMonth()+1) + '/' + dateObj.getFullYear();
 const dayHeader = document.querySelector('.day');
+
 //EVENT LISTENERS
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
+
+todoButtonTuesday.addEventListener("click", addTodoTuesday);
+todoListTuesday.addEventListener("click", deleteCheck);
+
+todoButtonWednesday.addEventListener("click", addTodoWednesday);
+todoListWednesday.addEventListener("click", deleteCheck);
+
+todoButtonThursday.addEventListener("click", addTodoThursday);
+todoListThursday.addEventListener("click", deleteCheck);
+
+todoButtonFriday.addEventListener("click", addTodoFriday);
+todoListFriday.addEventListener("click", deleteCheck);
+
 filterOptions.addEventListener("click", filterTodo);
 
 //FUNCTIONS
+
+//ADD TODO MONDAY
 function addTodo(event) {
     //Prevent form from submiting
     event.preventDefault();
@@ -26,7 +58,7 @@ function addTodo(event) {
 
     //Create li
     const newTodo = document.createElement('li');
-    newTodo.innerText = 'jira-prd/browse/' + todoInput.value;
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
 
     //Append li element to the list div
@@ -57,6 +89,175 @@ function addTodo(event) {
     todoInput.value = '';
 }
 
+//ADD TODO TUESDAY
+function addTodoTuesday(event) {
+    //Prevent form from submiting
+    event.preventDefault();
+
+    //Todo div 
+    const todoDivTuesday = document.createElement('div');
+    todoDivTuesday.classList.add('todo');
+
+    //Create li
+    const newTodoTuesday = document.createElement('li');
+    newTodoTuesday.innerText = todoInputTuesday.value;
+    newTodoTuesday.classList.add('todo-item');
+
+    //Append li element to the list div
+    todoDivTuesday.appendChild(newTodoTuesday);
+
+    //CHECKMARK BUTTON
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"> D / U </i>';
+    completedButton.classList.add('complete-btn');
+    todoDivTuesday.appendChild(completedButton);
+
+    //JIRA BUTTON
+    const jiraButton = document.createElement('button');
+    jiraButton.innerHTML = '<i class="fa-solid fa-cloud-arrow-up"></i>';
+    jiraButton.classList.add('jira-btn');
+    todoDivTuesday.appendChild(jiraButton);
+
+    //TRASH BUTTON
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDivTuesday.appendChild(trashButton);
+
+    //APPEND TO LIST
+    todoListTuesday.appendChild(todoDivTuesday);
+
+    //CLEAR TODO INPUT VALUE
+    todoInputTuesday.value = '';
+}
+
+//ADD TODO WEDNESDAY
+function addTodoWednesday(event) {
+    //Prevent form from submiting
+    event.preventDefault();
+
+    //Todo div 
+    const todoDivWednesday = document.createElement('div');
+    todoDivWednesday.classList.add('todo');
+
+    //Create li
+    const newTodoWednesday = document.createElement('li');
+    newTodoWednesday.innerText = todoInputWednesday.value;
+    newTodoWednesday.classList.add('todo-item');
+
+    //Append li element to the list div
+    todoDivWednesday.appendChild(newTodoWednesday);
+
+    //CHECKMARK BUTTON
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"> D / U </i>';
+    completedButton.classList.add('complete-btn');
+    todoDivWednesday.appendChild(completedButton);
+
+    //JIRA BUTTON
+    const jiraButton = document.createElement('button');
+    jiraButton.innerHTML = '<i class="fa-solid fa-cloud-arrow-up"></i>';
+    jiraButton.classList.add('jira-btn');
+    todoDivWednesday.appendChild(jiraButton);
+
+    //TRASH BUTTON
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDivWednesday.appendChild(trashButton);
+
+    //APPEND TO LIST
+    todoListWednesday.appendChild(todoDivWednesday);
+
+    //CLEAR TODO INPUT VALUE
+    todoInputWednesday.value = '';
+}
+
+//ADD TODO THURSDAY
+function addTodoThursday(event) {
+    //Prevent form from submiting
+    event.preventDefault();
+
+    //Todo div 
+    const todoDivThursday = document.createElement('div');
+    todoDivThursday.classList.add('todo');
+
+    //Create li
+    const newTodoThursday = document.createElement('li');
+    newTodoThursday.innerText = todoInputThursday.value;
+    newTodoThursday.classList.add('todo-item');
+
+    //Append li element to the list div
+    todoDivThursday.appendChild(newTodoThursday);
+
+    //CHECKMARK BUTTON
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"> D / U </i>';
+    completedButton.classList.add('complete-btn');
+    todoDivThursday.appendChild(completedButton);
+
+    //JIRA BUTTON
+    const jiraButton = document.createElement('button');
+    jiraButton.innerHTML = '<i class="fa-solid fa-cloud-arrow-up"></i>';
+    jiraButton.classList.add('jira-btn');
+    todoDivThursday.appendChild(jiraButton);
+
+    //TRASH BUTTON
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDivThursday.appendChild(trashButton);
+
+    //APPEND TO LIST
+    todoListThursday.appendChild(todoDivThursday);
+
+    //CLEAR TODO INPUT VALUE
+    todoInputThursday.value = '';
+}
+
+//ADD TODO FRIDAY
+function addTodoFriday(event) {
+    //Prevent form from submiting
+    event.preventDefault();
+
+    //Todo div 
+    const todoDivFriday = document.createElement('div');
+    todoDivFriday.classList.add('todo');
+
+    //Create li
+    const newTodoFriday = document.createElement('li');
+    newTodoFriday.innerText = todoInputFriday.value;
+    newTodoFriday.classList.add('todo-item');
+
+    //Append li element to the list div
+    todoDivFriday.appendChild(newTodoFriday);
+
+    //CHECKMARK BUTTON
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"> D / U </i>';
+    completedButton.classList.add('complete-btn');
+    todoDivFriday.appendChild(completedButton);
+
+    //JIRA BUTTON
+    const jiraButton = document.createElement('button');
+    jiraButton.innerHTML = '<i class="fa-solid fa-cloud-arrow-up"></i>';
+    jiraButton.classList.add('jira-btn');
+    todoDivFriday.appendChild(jiraButton);
+
+    //TRASH BUTTON
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDivFriday.appendChild(trashButton);
+
+    //APPEND TO LIST
+    todoListFriday.appendChild(todoDivFriday);
+
+    //CLEAR TODO INPUT VALUE
+    todoInputFriday.value = '';
+}
+
+
 //ADD DATE TO HEADER
 function getDate() {
     dayHeader.innerHTML = dayHeader.innerHTML + date;
@@ -65,7 +266,7 @@ function getDate() {
 }
 getDate();
 
-
+//BTN FUNCTION
 function deleteCheck(e) {
     const item = e.target;
     //delete todo item
@@ -90,8 +291,10 @@ function deleteCheck(e) {
     }
 }
 
+//FILTER LIST ITEMS
 
 function filterTodo(e) {
+    //FILTER MONDAY
     const todos = todoList.childNodes;
     todos.forEach(function(todo){
         if (todo.nodeType === 1) { // Check if it's an element node
@@ -100,7 +303,7 @@ function filterTodo(e) {
                     todo.style.display = 'flex';
                     break;
                 case "completed":
-                    if (todo.classList.contains('completed') && !todo.classList.contains('doneUp')) {
+                    if (todo.classList.contains('doneUp') && !todo.classList.contains('completed')) {
                         todo.style.display = 'flex';
                     } else {
                         todo.style.display = "none";
@@ -114,7 +317,147 @@ function filterTodo(e) {
                     }
                     break;
                 case "assigned":
-                    if (todo.classList.contains('doneUp')) {
+                    if (todo.classList.contains('completed')) {
+                        todo.style.display = 'flex';
+                    } else {
+                        todo.style.display = 'none';
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+
+    //FILTER TUESDAY
+    const todosTuesday = todoListTuesday.childNodes;
+    todosTuesday.forEach(function(todo){
+        if (todo.nodeType === 1) { // Check if it's an element node
+            switch(e.target.value) {
+                case "all":
+                    todo.style.display = 'flex';
+                    break;
+                case "completed":
+                    if (todo.classList.contains('doneUp') && !todo.classList.contains('completed')) {
+                        todo.style.display = 'flex';
+                    } else {
+                        todo.style.display = "none";
+                    }
+                    break;
+                case "uncompleted": 
+                    if (!todo.classList.contains('doneUp') && !todo.classList.contains('completed')) {
+                        todo.style.display = 'flex';
+                    } else {
+                        todo.style.display = "none";
+                    }
+                    break;
+                case "assigned":
+                    if (todo.classList.contains('completed')) {
+                        todo.style.display = 'flex';
+                    } else {
+                        todo.style.display = 'none';
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+
+    //FILTER WEDNESDAY
+    const todosWednesday = todoListWednesday.childNodes;
+    todosWednesday.forEach(function(todo){
+        if (todo.nodeType === 1) { // Check if it's an element node
+            switch(e.target.value) {
+                case "all":
+                    todo.style.display = 'flex';
+                    break;
+                case "completed":
+                    if (todo.classList.contains('doneUp') && !todo.classList.contains('completed')) {
+                        todo.style.display = 'flex';
+                    } else {
+                        todo.style.display = "none";
+                    }
+                    break;
+                case "uncompleted": 
+                    if (!todo.classList.contains('doneUp') && !todo.classList.contains('completed')) {
+                        todo.style.display = 'flex';
+                    } else {
+                        todo.style.display = "none";
+                    }
+                    break;
+                case "assigned":
+                    if (todo.classList.contains('completed')) {
+                        todo.style.display = 'flex';
+                    } else {
+                        todo.style.display = 'none';
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+
+    //FILTER THURSDAY
+    const todosThursday = todoListThursday.childNodes;
+    todosThursday.forEach(function(todo){
+        if (todo.nodeType === 1) { // Check if it's an element node
+            switch(e.target.value) {
+                case "all":
+                    todo.style.display = 'flex';
+                    break;
+                case "completed":
+                    if (todo.classList.contains('doneUp') && !todo.classList.contains('completed')) {
+                        todo.style.display = 'flex';
+                    } else {
+                        todo.style.display = "none";
+                    }
+                    break;
+                case "uncompleted": 
+                    if (!todo.classList.contains('doneUp') && !todo.classList.contains('completed')) {
+                        todo.style.display = 'flex';
+                    } else {
+                        todo.style.display = "none";
+                    }
+                    break;
+                case "assigned":
+                    if (todo.classList.contains('completed')) {
+                        todo.style.display = 'flex';
+                    } else {
+                        todo.style.display = 'none';
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+
+    //FILTER FRIDAY
+    const todosFriday = todoListFriday.childNodes;
+    todosFriday.forEach(function(todo){
+        if (todo.nodeType === 1) { // Check if it's an element node
+            switch(e.target.value) {
+                case "all":
+                    todo.style.display = 'flex';
+                    break;
+                case "completed":
+                    if (todo.classList.contains('doneUp') && !todo.classList.contains('completed')) {
+                        todo.style.display = 'flex';
+                    } else {
+                        todo.style.display = "none";
+                    }
+                    break;
+                case "uncompleted": 
+                    if (!todo.classList.contains('doneUp') && !todo.classList.contains('completed')) {
+                        todo.style.display = 'flex';
+                    } else {
+                        todo.style.display = "none";
+                    }
+                    break;
+                case "assigned":
+                    if (todo.classList.contains('completed')) {
                         todo.style.display = 'flex';
                     } else {
                         todo.style.display = 'none';
