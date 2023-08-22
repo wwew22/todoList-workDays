defe8ca704dac0f00ebc132943db816cffec4553
Alexandru Russe <russealexandru@gmail.com>
2023-08-22 10:49:58 +0300

    Chnages to design and added extra days

--- app.js
+++ app.js
@@ -4,10 +4,17 @@ const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 const filterOptions = document.querySelector('.filter-todo');
 
+//DATE VARs
+const dateObj = new Date();
+let dayName =['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let dayOfWeek = dayName[new Date().getDay()];
+let date =  dateObj.getDate() + '/'+ (dateObj.getMonth()+1) + '/' + dateObj.getFullYear();
+const dayHeader = document.querySelector('.day');
 //EVENT LISTENERS
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
 filterOptions.addEventListener("click", filterTodo);
+
 //FUNCTIONS
 function addTodo(event) {
     //Prevent form from submiting
@@ -19,7 +26,7 @@ function addTodo(event) {
 
     //Create li
     const newTodo = document.createElement('li');
-    newTodo.innerText = todoInput.value;
+    newTodo.innerText = 'jira-prd/browse/' + todoInput.value;
     newTodo.classList.add('todo-item');
 
     //Append li element to the list div
@@ -27,9 +34,15 @@ function addTodo(event) {
 
     //CHECKMARK BUTTON
     const completedButton = document.createElement('button');
-    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.innerHTML = '<i class="fas fa-check"> D / U </i>';
     completedButton.classList.add('complete-btn');
     todoDiv.appendChild(completedButton);
+
+    //JIRA BUTTON
+    const jiraButton = document.createElement('button');
+    jiraButton.innerHTML = '<i class="fa-solid fa-cloud-arrow-up"></i>';
+    jiraButton.classList.add('jira-btn');
+    todoDiv.appendChild(jiraButton);
 
     //TRASH BUTTON
     const trashButton = document.createElement('button');
@@ -43,6 +56,15 @@ function addTodo(event) {
     //CLEAR TODO INPUT VALUE
     todoInput.value = '';
 }
+
+//ADD DATE TO HEADER
+function getDate() {
+    dayHeader.innerHTML = dayHeader.innerHTML + date;
+    console.log(dayHeader.innerHTML);
+    
+}
+getDate();
+
 
 function deleteCheck(e) {
     const item = e.target;
@@ -58,6 +80,12 @@ function deleteCheck(e) {
     //CHECKMARK
     if (item.classList[0]=== 'complete-btn') {
         const todo = item.parentElement;
+        todo.classList.toggle("doneUp");
+    }
+
+    //DONE AND UP
+    if (item.classList[0]=== 'jira-btn') {
+        const todo = item.parentElement;
         todo.classList.toggle("completed");
     }
 }
@@ -72,17 +100,24 @@ function filterTodo(e) {
                     todo.style.display = 'flex';
                     break;
                 case "completed":
-                    if (todo.classList.contains('completed')) {
+                    if (todo.classList.contains('completed') && !todo.classList.contains('doneUp')) {
                         todo.style.display = 'flex';
                     } else {
                         todo.style.display = "none";
                     }
                     break;
-                case "uncompleted": // Add this case for handling uncompleted tasks
-                    if (!todo.classList.contains('completed')) {
+                case "uncompleted": 
+                    if (!todo.classList.contains('doneUp') && !todo.classList.contains('completed')) {
                         todo.style.display = 'flex';
                     } else {
                         todo.style.display = "none";
+                    }
+                    break;
+                case "assigned":
+                    if (todo.classList.contains('doneUp')) {
+                        todo.style.display = 'flex';
+                    } else {
+                        todo.style.display = 'none';
                     }
                     break;
                 default:
